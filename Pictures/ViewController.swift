36c377622c9d5ecbda235d0e2e9fe23a3fe0cb2f
krod504 <krod3584@gmail.com>
2020-06-09 10:42:17 -0400

    Initial Commit

---
+++ Pictures/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pictures
+//
+//  Created by Karla Rodriguez on 6/9/20.
+//  Copyright © 2020 Karla Rodriguez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
